bf55db0fca01f38e7c7645d938473906d8cb3fbb
Peng Xiao <pengxiao@outlook.com>
2022-03-24 11:35:14 +0800

    fix: fix most issues for stable release

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # vite-plugin-logseq
 
+A Vite plugin that is essential for developing Vite plugins locally. 
+
+It should fix the follow HMR issues for you:
+- a valid index.html for local development
+- reload the plugin if update is outside of your UI framework's HMR boundary 
+
 ## Install
 
 `npm install vite-plugin-logseq`
@@ -16,3 +22,5 @@ plugins: [
   logseqPlugin()
 ]
 ```
+
+[MIT](/LICENSE) @pengx17
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { ConfigEnv, Plugin, ResolvedConfig, ViteDevServer } from "vite";
 import { writeFile, mkdir } from "fs/promises";
 import path from "path";
 import fs from "fs";
+import MagicString from "magic-string";
 import { RequestOptions, get as httpGet } from "http";
 
 const pluginName = "vite:logseq-dev-plugin";
@@ -44,22 +45,14 @@ const logseqDevPlugin: () => Plugin = () => {
   let server: ViteDevServer;
   const pluginId = getLogseqPluginId();
 
-  const delay = (ms: number) =>
-    new Promise((resolve) => setTimeout(resolve, ms));
-
   // eagerly load the HTML file to let it write to the dist
-  const tapHtml = async (config: ResolvedConfig) => {
-    await delay(1000);
-
-    const res = await request(
-      `http://${config.server.host}:${config.server.port}`,
-      {
-        method: "GET",
-        headers: {
-          accept: "text/html",
-        },
-      }
-    );
+  const tapHtml = async (address: string) => {
+    const res = await request(address, {
+      method: "GET",
+      headers: {
+        accept: "text/html",
+      },
+    });
     return res;
   };
 
@@ -67,20 +60,23 @@ const logseqDevPlugin: () => Plugin = () => {
     name: pluginName,
     enforce: "post",
     config: async (config, resolvedEnv) => {
-      if (
-        resolvedEnv.command === "serve" &&
-        (!config.server?.host ||
-          !config.server?.port ||
-          !config.server?.strictPort)
-      ) {
-        throw new Error(
-          `${pluginName} requires server.host, server.port, and server.strictPort to be set for dev mode`
-        );
-      }
       configEnv = resolvedEnv;
+
       // make sure base is empty, otherwise when running in build mode
       // the asset will be served from the root "/"
       config.base = "";
+
+      // Plugin works in file://, but it fetches vite resources from localhost
+      // thus we must turn on cors
+      if (resolvedEnv.command === "serve") {
+        config.server = Object.assign({}, config.server, {
+          cors: true,
+          hmr: {
+            host: config.server?.host ?? "localhost",
+          },
+        });
+      }
+
       return config;
     },
 
@@ -99,38 +95,57 @@ const logseqDevPlugin: () => Plugin = () => {
         !/node_modules/.test(id) &&
         id.startsWith(process.cwd())
       ) {
-
-        // amend entries
-        return (
+        const s = new MagicString(code);
+        s.prepend(
           `
-        import.meta.hot.accept(() => {});
-        import.meta.hot.dispose(() => {
-          top?.LSPluginCore.reload("${pluginId}");
-          console.log("✨Plugin ${pluginId} reloaded ✨");
-        });\n\n` + code
+if (import.meta.hot) {
+  import.meta.hot.accept(() => {});
+  import.meta.hot.dispose(() => {
+    top?.LSPluginCore.reload("${pluginId}");
+    console.log("✨Plugin ${pluginId} reloaded ✨");
+    // TODO: trigger re-render globally
+  });
+}\n\n`
         );
+        // amend entries
+        return {
+          code: s.toString(),
+          map: s.generateMap({ hires: true }),
+        };
       }
     },
 
     // Overwrite dev HTML
-    async buildStart(opt) {
+    async buildStart() {
       if (configEnv.command === "serve") {
-        tapHtml(config).then(async (html) => {
-          // Rewrite the base, otherwise assets like `/@vite/client` will
-          // subject to default `file://` path
-          const baseHref = `http://${config.server.host}:${config.server.port}`;
-          const baseString = `<base href="${baseHref}">`;
-          const htmlWithBase = html.replace(`<head>`, `<head>${baseString}`);
-
-          await mkdir(config.build.outDir, { recursive: true });
-          await writeFile(
-            path.resolve(config.build.outDir, "index.html"),
-            htmlWithBase,
-            {
-              encoding: "utf-8",
-            }
+        if (!server.httpServer) {
+          throw new Error(
+            `${pluginName} Only works for non-middleware mode for now`
           );
-          console.info(`${pluginName}: Wrote development index.html`);
+        }
+
+        server.httpServer.once("listening", () => {
+          let address = server.httpServer!.address()!;
+          if (typeof address === "object" && address) {
+            address = "http://" + address.address + ":" + address.port;
+          }
+          tapHtml(address).then(async (html) => {
+            // Rewrite the base, otherwise assets like `/@vite/client` will
+            // subject to default `file://` path
+            const baseHref = address;
+            const baseString = `<base href="${baseHref}">`;
+            const htmlWithBase = html.replace(`<head>`, `<head>${baseString}`);
+
+            await mkdir(config.build.outDir, { recursive: true });
+            await writeFile(
+              path.resolve(config.build.outDir, "index.html"),
+              htmlWithBase,
+              {
+                encoding: "utf-8",
+              }
+            );
+            console.info(`${pluginName}: Wrote development index.html`);
+          });
         });
       }
     },
